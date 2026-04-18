eab210a08f9978a12cf0a4b289912527cc7c5bb1
moinahalima-abdou <moina-halima.abdou-doukeini@laplateforme.io>
2026-04-18 09:57:14 +0200

    feat : new foctinality app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+
+import matplotlib.pyplot as plt
 import streamlit as st
 from services.weather_api import get_weather
 from utils.history import History
@@ -15,14 +17,47 @@ if st.button("Rechercher"):
         st.error(data["error"])
     else:
         st.success(f"Météo à {data['city']}")
+
+       
+        # 📊 AFFICHAGE DONNÉES
+        
         st.write(f"🌡️ Température : {data['temp']}°C")
         st.write(f"☁️ Description : {data['description']}")
         st.write(f"💨 Vent : {data['wind']} m/s")
         st.write(f"💧 Humidité : {data['humidity']}%")
 
+       
+        # 💾 HISTORIQUE
+        
         history.add_city(city)
 
-# affichage historique
+        
+        # 📊 GRAPHIQUES
+       
+        labels = ["Température", "Humidité", "Vent"]
+        values = [data["temp"], data["humidity"], data["wind"]]
+
+        st.subheader("📊 Graphiques météo")
+
+        # --- BAR CHART ---
+        st.write("📊 Graphique en barres")
+        fig1, ax1 = plt.subplots()
+        ax1.bar(labels, values)
+        ax1.set_title(f"Météo de {data['city']}")
+        st.pyplot(fig1)
+
+        # --- LINE CHART ---
+        st.write("📈 Graphique en courbe (bonus)")
+        fig2, ax2 = plt.subplots()
+        ax2.plot(labels, values, marker="o")
+        ax2.set_title(f"Tendance météo de {data['city']}")
+        st.pyplot(fig2)
+
+
+# 📜 HISTORIQUE
+
+st.subheader("📜 Historique")
+
 if st.checkbox("Voir l'historique"):
     hist = history.load_history()
 
