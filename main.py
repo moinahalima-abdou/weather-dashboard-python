69fca63de35877417d7c333e0d23bed561f3798e
moinahalima-abdou <moina-halima.abdou-doukeini@laplateforme.io>
2026-04-18 08:53:20 +0200

    feat : add history display in main menu

--- main.py
+++ main.py
@@ -3,32 +3,50 @@ from models.weather import Weather
 from utils.history import History
 
 
+def show_menu():
+    print("\n===== WEATHER APP =====")
+    print("1. Rechercher une ville")
+    print("2. Voir l'historique")
+    print("3. Quitter")
+
+
 def main():
     history = History()
 
     while True:
-        city = input("\n🌍 Entrer une ville (ou 'exit'): ")
+        show_menu()
+        choice = input("Choisis une option : ")
 
-        if city.lower() == "exit":
+        if choice == "1":
+            city = input("🌍 Entrer une ville : ")
+
+            data = get_weather(city)
+
+            if "error" in data:
+                print("❌ Erreur:", data["error"])
+                continue
+
+            weather = Weather(
+                city=data["city"],
+                temp=data["temp"],
+                description=data["description"],
+                wind=data["wind"],
+                humidity=data["humidity"]
+            )
+
+            print("\n" + str(weather))
+
+            history.add_city(city)
+
+        elif choice == "2":
+            history.show_history()
+
+        elif choice == "3":
+            print("Au revoir 👋")
             break
 
-        data = get_weather(city)
-
-        if "error" in data:
-            print("❌ Erreur:", data["error"])
-            continue
-
-        weather = Weather(
-            city=data["city"],
-            temp=data["temp"],
-            description=data["description"],
-            wind=data["wind"],
-            humidity=data["humidity"]
-        )
-
-        print("\n" + str(weather))
-
-        history.add_city(city)
+        else:
+            print("Option invalide")
 
 
 if __name__ == "__main__":
